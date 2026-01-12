79cd5008b9f64c1ba92c3f17db95f4157636b86e
Lisandra <lisa.crisilva@outlook.com>
2026-01-12 20:30:13 -0300

    onUpgrade e onDowngrade db

--- lib/database/database_sqlite.dart
+++ lib/database/database_sqlite.dart
@@ -2,13 +2,17 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseSqLite {
-  Future<void> openConnection() async {
+  Future<Database> openConnection() async {
     final databasePath = await getDatabasesPath();
     final databaseFinalPath = join(databasePath, 'SQULITE_EXAMPLE');
 
-    await openDatabase(
+    return await openDatabase(
       databaseFinalPath,
-      version: 1,
+      version: 2,
+      onConfigure: (db) async {
+        print('onConfigure sendo chamado');
+        await db.execute('PRAGMA foreing_keys = ON');
+      },
       // chamado só no momento de criação do bd, quando baixa o app
       onCreate: (Database db, int version) {
         final batch = db.batch();
@@ -19,12 +23,67 @@ class DatabaseSqLite {
             nome varchar(200)
           )
         ''');
+
+        batch.execute('''
+          create table produto(
+            id Integer primary key autoincrement,
+            nome varchar(200)            
+          )
+        ''');
+
+        // batch.execute('''
+        //   create table categoria(
+        //     id Integer primary key autoincrement,
+        //     nome varchar(200)
+        //   )
+        // ''');
+
         batch.commit();
       },
       // chamado quando houver alteração no version incremental (1 -> 2)
-      onUpgrade: (Database db, int oldVersion, int version) {},
+      onUpgrade: (Database db, int oldVersion, int version) {
+        print('onUpgrade chamado');
+        final batch = db.batch();
+
+        if (oldVersion == 1) {
+          batch.execute('''
+            create table produto(
+              id Integer primary key autoincrement,
+              nome varchar(200)
+            )
+          ''');
+          // batch.execute('''
+          //   create table categoria(
+          //     id Integer primary key autoincrement,
+          //     nome varchar(200)
+          //   )
+          // ''');
+        }
+
+        // if (oldVersion == 2) {
+        //   batch.execute('''
+        //     create table categoria(
+        //       id Integer primary key autoincrement,
+        //       nome varchar(200)
+        //     )
+        //   ''');
+        // }
+
+        batch.commit();
+      },
       // chamado quando houver alteração no version decremental (2 -> 1)
-      onDowngrade: (Database db, int oldVersion, int version) {},
+      onDowngrade: (Database db, int oldVersion, int version) {
+        print('onDowngrade chamado');
+        final batch = db.batch();
+
+        if (oldVersion == 3) {
+          batch.execute('''
+            drop table categoria;
+          ''');
+        }
+
+        batch.commit();
+      },
     );
   }
 }
