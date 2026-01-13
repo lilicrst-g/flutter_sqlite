2ebad425b092490599c919e0badc71f7da1a2652
Lisandra <lisa.crisilva@outlook.com>
2026-01-12 21:06:26 -0300

    manipulação de dados

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,7 +12,17 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    DatabaseSqLite().openConnection();
+    _database();
+  }
+
+  Future<void> _database() async {
+    final database = await DatabaseSqLite().openConnection();
+
+    database.rawInsert('insert into teste values(null, ?)', ['Rita']);
+    // database.rawUpdate('update teste set nome = ? where id = ?', ['Lis', 1]);
+    // database.rawDelete('delete from teste where id = ?', [3]);
+    // var result = await database.rawQuery('select * from teste');
+    // print(result);
   }
 
   @override
